c2c7dbff5b0aaf530f25d50e6582d6b90515bd15
Breno Valentim <brenosuvale@gmail.com>
2024-10-27 15:56:04 -0300

    Modo-escuro main.js
    Coloquei o js do manchete para o botão do modo escuro

---
+++ Scripts/main.js
@@ -0,0 +1,12 @@
+function toggleDarkMode() {
+  
+    var body = document.body;
+    var darkModeToggle = document.getElementById("dark-mode-toggle");
+  
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        darkModeToggle.textContent = "☀️";
+    } else {
+        darkModeToggle.textContent = "🌙";
+    }
+}
