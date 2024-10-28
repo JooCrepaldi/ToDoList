22572041eb6b29cc08448b1137d4c7fd16f6ea84
Breno Valentim <brenosuvale@gmail.com>
2024-10-28 17:20:14 -0300

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,21 +11,28 @@ function toggleDarkMode() {
     }
 }
 
+/*Função que irá adicionar uma nova linha conforme  o usuário digitar algo no campo de texto*/
+
 function addNewLine(){
-    var inputValue = document.getElementById("input").value;
     // Variável inputValue captura o texto do input quando o botão "+" é pressionado
+    var inputValue = document.getElementById("input").value;
+    //Variável taskList é para ir adicionando as tarefas na lista conforme o usuário digita algo no campo de texto
     var taskList = document.getElementById("taskList");
+    //checkbox funcionando como  um botão para marcar a tarefa como concluída
     var checkbox = document.createElement("button");
     checkbox.classList.add("checkbox");
     checkbox.onclick = function(){
+        //se a checkbox (que está pressionada) for igual a 1 (ou ao simbolo do vetor certo),  a tarefa é concluída e o simbolo some
+
         if(checkbox.textContent == "✅"){
             checkbox.textContent = '';
         }
+        //se não, ele permanece  como está
         else{
             checkbox.textContent = "✅";
         }
     }
-    
+    //se o valor do input for diferente de um espaço em branco, vai adicionar uma linha, com uma checkbox  e o texto do input
     if(inputValue.trim() != ''){
         taskList = document.createElement("li");
         taskList.appendChild(checkbox);
