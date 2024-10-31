02a043b55b0df58f61396ab47526cafae4aac1c0
Fabricio Krull <fabricioakrull@gmail.com>
2024-10-30 21:51:46 -0300

    Crepaldi, o pai do CSS

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,13 +5,9 @@ var evenOrOdd2 = 0;
 function color(){
     if(document.getElementById("theme-icon").textContent == "dark_mode"){
         document.getElementById("theme-icon").textContent = "light_mode";
-        body.classList.remove("dark_mode");
-        body.classList.add("light_mode");
     }
     else{
         document.getElementById("theme-icon").textContent = "dark_mode";
-        body.classList.remove("light_mode");
-        body.classList.add("dark_mode");
     }
     
 }
@@ -82,7 +78,6 @@ function addNewLine(){
     document.getElementById("input").value = '';
 }
 
-/*função para alternar entre as opções de tarefas concluídas, importantes e normais*/
 function switchScreen(){
     document.getElementById("input").value = '';
     (document.getElementById("input")).style.display = "block";
